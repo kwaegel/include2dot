1474f695364ee57ef3f07e50038041168b8e4886
Ky Waegel <kwaegel@users.noreply.github.com>
2016-10-23 21:02:28 -0700

    Find absolute path of includes

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use clap::{Arg, App};
 
 use std::io::{self, Read};
 use std::fs::File;
-use std::path::Path;
+use std::path::{Path, PathBuf};
 use std::ffi::OsStr;
 
 use std::collections::HashSet;
@@ -19,6 +19,46 @@ extern crate regex;
 
 use regex::Regex;
 
+#[derive(Debug)]
+enum IncludeStatus {
+    Relative,     // e.g. <vector>
+    Absolute,     // e.g. /usr/include/c++/4.8/vector
+    FailedLookup, // Failed to locate an absolute include path.
+}
+
+#[derive(Debug)]
+struct Include {
+    path: PathBuf,
+    is_system_include: bool,
+    status: IncludeStatus,
+}
+
+impl Include {
+    fn new(path: &Path, is_sys: bool, status: IncludeStatus) -> Include {
+        Include{path: PathBuf::from(path),
+            is_system_include: is_sys,
+            status: status}
+    }
+
+    fn new_relative(name: &str, is_sys: bool) -> Include {
+        Include{path: PathBuf::from(name),
+            is_system_include: is_sys,
+            status: IncludeStatus::Relative}
+    }
+
+    fn as_absolute(&self, absolute_path: &Path) -> Include {
+        Include{path: PathBuf::from(absolute_path),
+            is_system_include: self.is_system_include,
+            status: IncludeStatus::Absolute}
+    }
+
+    fn as_failed_lookup(&self) -> Include {
+        Include{path: PathBuf::from(&self.path),
+            is_system_include: self.is_system_include,
+            status: IncludeStatus::FailedLookup}
+    }
+}
+
 fn is_hidden(entry: &DirEntry) -> bool {
     entry.file_name()
         .to_str()
@@ -26,9 +66,25 @@ fn is_hidden(entry: &DirEntry) -> bool {
         .unwrap_or(false)
 }
 
+// Convert a relative include path (e.g. <vector>) into an absolute path for hashing.
+fn find_absolute_include_path(include: &Include, local_search_path: &Path, search_paths: &[PathBuf]) -> Include {
+
+    let local_full_path = local_search_path.join(&include.path);
+    if local_full_path.exists() {
+        return include.as_absolute(&local_full_path);
+    }
+
+    for search_prefix in search_paths {
+        let full_path = search_prefix.join(&include.path);
+        if full_path.exists() {
+            return include.as_absolute(&full_path);
+        }
+    }
+    include.as_failed_lookup()
+}
 
 // Return a list of #include statements found in the file
-fn scan_for_includes(file: &Path) -> Result<Vec<String>, io::Error> {
+fn scan_file_for_includes(file: &Path) -> Result<Vec<Include>, io::Error> {
     let mut f = try!(File::open(file));
     let mut text = String::new();
     try!(f.read_to_string(&mut text));
@@ -38,12 +94,16 @@ fn scan_for_includes(file: &Path) -> Result<Vec<String>, io::Error> {
     // Use a regex to search for '#include ...' lines.
     // The (...) capture group isolates just the text, not the "" or <> symbols.
     lazy_static! {
-        static ref RE: Regex = Regex::new(r##"#include [<|"](.*)[>|"]"##).unwrap();
+        static ref RE: Regex = Regex::new(r##"#include ([<|"])(.*)[>|"]"##).unwrap();
     }
 
+    // cap.at(1) is an angle brace or double quote, to determine user or system include.
+    // cap.at(2) is the include file name.
     for cap in RE.captures_iter(&text) {
-        match cap.at(1) {
-            Some(include_str) => includes.push(include_str.to_owned()),
+        let inc_symbol = cap.at(1).unwrap_or("<");
+        let is_system_include = inc_symbol == "<";
+        match cap.at(2) {
+            Some(include_name) => includes.push(Include::new_relative(include_name, is_system_include)),
             None => {}
         }
     }
@@ -82,6 +142,10 @@ fn main() {
         println!("Unable to access directory: {}", root_dir.display());
     }
 
+    // Create a list of default system include paths.
+    let mut search_paths = Vec::new();
+    search_paths.push(PathBuf::from(r"C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\include"));
+
     // Restrict the file extensions to search.
     let mut extensions = HashSet::new();
     extensions.insert(OsStr::new("h"));
@@ -90,11 +154,11 @@ fn main() {
 
     let mut files: HashSet<String> = HashSet::new();
 
-
+    // Collect all the files to scan
     let walker = WalkDir::new(root_dir).into_iter();
     for entry in walker.filter_entry(|e| !is_hidden(e)) {
         let entry = entry.unwrap(); // What error values could there be?
-        let path = entry.path();
+        let path = entry.path().canonicalize().unwrap();
 
         // If the file extension matches our set, queue full path for processing.
         match path.extension() {
@@ -107,25 +171,34 @@ fn main() {
         }
     }
 
+    // Maps source_path => Vec<include_path>
     let mut include_table = HashMap::new();
 
-    for path in files {
-        let includes_result = scan_for_includes(Path::new(&path));
-        // println!("{:?}", includes_result);
+    for path_str in files {
+        let path = Path::new(&path_str);
+        let includes_result = scan_file_for_includes(path);
         match includes_result {
             Ok(includes) => {
-                include_table.insert(path.clone(), includes);
+                let local_dir = path.parent().unwrap(); // strip the file name
+
+                // Convert relative includes to absolute includes
+                let absolute_includes: Vec<_> = includes.iter()
+                    .map(|inc| find_absolute_include_path(inc, local_dir, &search_paths))
+                    .collect();
+
+                include_table.insert(PathBuf::from(path), absolute_includes);
             }
             Err(err) => {
-                println!("Unable to process file {}: {}", path, err);
+                println!("Unable to process file {:?}: {}", path, err);
             }
         };
     }
 
-    for (file, includes) in include_table {
-        println!("{}", file);
+    // Print all the includes
+    for (file_path, includes) in include_table {
+        println!("{:?}", file_path);
         for inc in includes {
-            println!("  {}", inc);
+            println!("  {:?}", inc);
         }
     }
 }
