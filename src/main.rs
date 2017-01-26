846cb876fae438a74979f8440c96f5e15c2ff2e7
Ky Waegel <kwaegel@users.noreply.github.com>
2017-01-26 00:44:49 -0800

    Moved directory traversal loop to a submodule
    * Should make automated testing easier

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,7 @@
 extern crate clap;
 use clap::{Arg, App};
 
-use std::io::{self, Read};
-use std::fs::File;
-use std::path::{Path, PathBuf};
+use std::path::PathBuf;
 use std::ffi::OsString;
 use std::error::Error;
 use std::env;
@@ -13,9 +11,9 @@ use std::env;
 use std::collections::HashSet;
 
 extern crate petgraph;
-
 extern crate walkdir;
-use walkdir::WalkDir;
+extern crate itertools;
+
 
 #[macro_use]
 extern crate lazy_static;
@@ -24,74 +22,13 @@ extern crate regex;
 use regex::Regex;
 
 mod path_utils;
-
 mod dot_writer;
-
 mod file_node;
-use file_node::FileNode;
-
 mod hash_graph;
-use hash_graph::HashGraph;
 
-extern crate itertools;
-use itertools::Itertools;
+mod find_includes;
+use find_includes::find_includes_in_tree;
 
-// ----------------------------------------------------------------------------
-
-// Convert a relative include path (e.g. <Windows.h>) into an absolute path.
-fn find_absolute_include_path(include: &FileNode,
-                              parent_file: &Path,
-                              system_search_paths: &[PathBuf])
-                              -> FileNode {
-
-    let local_dir = parent_file.parent().unwrap(); // strip the file name
-
-    let normalized_path = path_utils::normalize_path_separators(&include.path);
-
-    match path_utils::convert_to_absolute_path(&normalized_path, local_dir, system_search_paths) {
-        None => {
-
-            println!("Unable to locate {:?}", &include.path);
-            println!("Included from file {:?}\n", parent_file);
-            include.clone()
-        }
-        Some(path_buf) => FileNode::from_path(&path_buf, include.is_system),
-    }
-}
-
-// -----------------------------------------------------------------------------
-
-// Return a list of #include statements found in the file
-fn scan_file_for_includes(file: &Path) -> Result<Vec<FileNode>, io::Error> {
-    let mut f = File::open(file)?;
-    let mut text = String::new();
-    f.read_to_string(&mut text)?;
-
-    let mut includes = Vec::new();
-
-    // Use a regex to search for '#include ...' lines.
-    // The second (...) capture group isolates just the text, not the "" or <> symbols.
-    lazy_static! {
-        // Notes:
-        // (?m:^[[:blank:]]*) => empty space at line start, multi-line mode, non-capturing group.
-        static ref RE: Regex =
-        Regex::new(r##"(?m:^[[:blank:]]*)#[[:blank:]]*include[[:blank:]]*([<"])(.*)[>"]"##).unwrap();
-    }
-
-    // cap.at(1) is an angle brace or double quote, to determine user or system include.
-    // cap.at(2) is the include file name.
-    if let Some(cap) = RE.captures(&text) {
-        let is_system_include = cap.get(1).map_or(false, |sym| sym.as_str() == "<");
-
-        if let Some(include_name) = cap.get(2) {
-            includes.push(FileNode::new(include_name.as_str(), is_system_include));
-        }
-    }
-
-    // println!("Found {} includes in {}", includes.len(), &file.display());
-
-    Ok(includes)
-}
 
 // -----------------------------------------------------------------------------
 
@@ -132,57 +69,6 @@ arg_enum! {
         angle,
         quote
     }
-}
-
-// Core include searching loop
-fn find_includes(root_dir: &Path,
-                 search_paths: &Vec<PathBuf>,
-                 extensions: &HashSet<OsString>,
-                 parse_user_includes: bool,
-                 parse_system_includes: bool,
-                 exclude_regex: &Option<Regex>) -> HashGraph<FileNode> {
-    // Collect all the files to scan in a HashSet
-    // Note: is_hidden() is currently hiding paths that start with './', so don't use it yet.
-    let input_queue = WalkDir::new(root_dir).into_iter()
-        //.filter_entry(|e| !path_utils::is_hidden(e))
-        .filter_map(|entry| match entry {
-            Err(what) => {println!("Error reading directory: {}", what.description()); None},
-            Ok(val) => Some(val),
-        })
-        .map(|entry| PathBuf::from(entry.path()))
-        .filter(|path| path.extension().map_or(false, |ext| extensions.contains(ext)))
-        .filter(|path| !path_utils::filename_matches_regex(&exclude_regex, path))
-        .collect::<HashSet<_>>();
-
-    // Graph of all the tracked files
-    let mut hash_graph = HashGraph::<FileNode>::new();
-
-    for path_buf in input_queue {
-        let parent_file = path_buf.as_path();
-        let includes_result = scan_file_for_includes(parent_file);
-        match includes_result {
-            Ok(includes) => {
-
-                // Convert relative includes to absolute includes
-                includes.iter()
-                    .filter(|inc| (!inc.is_system && parse_user_includes)
-                        || (inc.is_system && parse_system_includes))
-                    .filter(|inc| !path_utils::name_matches_regex(&exclude_regex, &inc.path))
-                    .map(|inc| find_absolute_include_path(inc, parent_file, &search_paths))
-                    .foreach(|inc| {
-                        // Add an edge to the graph
-                        let src_node = FileNode::from_path(parent_file, false);
-                        let dst_node = FileNode::from_path(&inc.path,inc.is_system);
-                        hash_graph.add_edge(src_node, dst_node);
-                    });
-            }
-            Err(err) => {
-                println!("Unable to process file {:?}: {}", parent_file, err);
-            }
-        }
-    }
-
-    hash_graph
 }
 
 
@@ -287,14 +173,13 @@ fn main() {
                 .ok() // Converts successful result to Some(), discarding errors.
         });
 
-    let hash_graph = find_includes(&root_dir,
-                                   &search_paths,
-                                   &extensions,
-                                   parse_user_includes,
-                                   parse_system_includes,
-                                   &exclude_regex);
-
-
+    let hash_graph = find_includes_in_tree(&root_dir,
+                                           &search_paths,
+                                           &extensions,
+                                           parse_user_includes,
+                                           parse_system_includes,
+                                           &exclude_regex);
+    
     // Write the graph to a dot file.
     let _ = dot_writer::write_dot_with_header("./graph.dot", &hash_graph.graph);
 
